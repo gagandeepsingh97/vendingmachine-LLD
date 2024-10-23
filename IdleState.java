f7d3e9f623dcd579de772fac764c72f41ebe2db4
gagandeepsingh97 <gagandeepsingh2911@gmail.com>
2024-10-22 22:48:53 -0400

    Updated class IdleState

--- IdleState.java
+++ IdleState.java
@@ -6,18 +6,33 @@ public class IdleState implements VendingMachineState {
     }
 
     @Override
-    void selectProduct(Product product) {
+    public void selectProduct(Product product) {
         if (vendingMachine.Inventory.isAvailable(product)) {
             vendingMachine.setSelectedProduct(product);
-
+            vendingMachine.setState(vendingMachine.getReadyState());
+            System.out.println("Product selected: " + product.getName());
+        } else {
+            System.out.println("Product not available: " + product.getName());
         }
     }
 
-    void insertCoin(Coin coin);
+    @Override
+    public void insertCoin(Coin coin) {
+        System.out.println("Please select a product first.");
+    }
 
-    void insertNote(Note note);
+    @Override
+    public void insertNote(Note note) {
+        System.out.println("Please select a product first.");
+    }
 
-    void dispenseProduct();
+    @Override
+    public void dispenseProduct() {
+        System.out.println("Please select a product and make payment.");
+    }
 
-    void returnChange();
+    @Override
+    public void returnChange() {
+        System.out.println("No change to return.");
+    }
 }
